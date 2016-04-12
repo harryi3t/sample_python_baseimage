d765f7375d71365af663584e9ad6de7a5d0c2660
Harendra Singh <66hari@gmail.com>
2016-04-12 15:04:18 +0530

    Adds 100 failing test case

--- test.py
+++ test.py
@@ -4,13 +4,319 @@ from app import App
 class TestSuite(unittest.TestCase):
     def test(self):
         app = App()
-        self.failIf(app.calculate(1,2) != 3)
+        self.failIf(app.calculate(1,1) != 2)
+    def test1(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
     def test2(self):
         app = App()
-        self.failIf(app.calculate(1,2) != 5)
+        self.failIf(app.calculate(0,0) != 1)
     def test3(self):
         app = App()
-        self.failIf(app.calculate(1,2) != 3)
+        self.failIf(app.calculate(0,0) != 1)
+    def test4(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test5(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test6(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test7(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test8(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test9(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test10(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test11(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test12(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test13(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test14(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test15(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test16(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test17(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test18(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test19(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test20(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test21(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test22(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test23(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test24(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test25(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test26(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test27(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test28(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test29(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test30(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test31(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test32(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test33(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test34(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test35(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test36(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test37(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test38(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test39(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test40(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test41(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test42(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test43(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test44(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test45(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test46(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test47(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test48(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test49(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test10(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test51(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test52(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test53(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test54(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test55(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test56(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test57(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test58(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test59(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test60(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test61(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test62(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test63(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test64(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test65(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test66(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test67(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test68(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test69(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test70(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test71(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test72(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test73(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test74(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test75(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test76(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test77(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test78(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test79(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test80(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test81(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test82(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test83(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test84(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test85(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test86(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test87(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test88(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test89(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test90(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+    def test91(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test92(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test93(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test94(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test95(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test96(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test97(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test98(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test99(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+    def test100(self):
+        app = App()
+        self.failIf(app.calculate(0,0) != 1)
+
+
+
 
 def main():
     unittest.main()
